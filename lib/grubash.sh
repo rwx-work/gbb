280c90779f5913bbf05551c6e16fd2c939af28c6
Marc Beninca <grubash@rwx.work>
2021-11-08 03:17:32 +0100

    grubash_wipe

--- lib/grubash.sh
+++ lib/grubash.sh
@@ -2,3 +2,16 @@ GRUBASH_BIOS_DIRECTORY='bios'
 
 GRUBASH_BIOS_IMAGE="${GRUBASH_BIOS_DIRECTORY}/core.img"
 GRUBASH_BIOS_SETUP="${GRUBASH_BIOS_DIRECTORY}/setup"
+
+function grubash_wipe {
+remove \
+"${EFI_ROOT}" \
+"${GRUBASH_BIOS_DIRECTORY}" \
+"${GRUB_IMAGE_ROOT}"
+}
+
+function grubash_clean {
+remove \
+"${GRUB_IMAGE_ARCHIVE}" \
+"${GRUB_IMAGE_ROOT}"
+}

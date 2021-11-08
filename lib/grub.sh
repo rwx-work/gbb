5084e1d50a7cdc15566885582c78e2abb3985e38
Marc Beninca <grubash@rwx.work>
2021-11-08 03:39:27 +0100

    grub_make_memdisk

--- lib/grub.sh
+++ lib/grub.sh
@@ -10,6 +10,28 @@ GRUB_IMAGE_ARCHIVE="${GRUB_IMAGE_ROOT}.tar"
 GRUB_BIOS_SETUP="${GRUB_BIOS}/grub-bios-setup"
 GRUB_IMAGE_FILE="${GRUB_IMAGE_DIRECTORY}/grub.cfg"
 
+function grub_make_memdisk {
+local esp_uuid="${1}"
+
+remove "${GRUB_IMAGE_ROOT}"
+make_directory "${GRUB_IMAGE_DIRECTORY}"
+
+echo -n "\
+search --set root --fs-uuid ${esp_uuid}
+unset prefix
+pager=1
+normal
+" >> "${GRUB_IMAGE_FILE}"
+
+tar \
+--create \
+--auto-compress \
+--file "${GRUB_IMAGE_ARCHIVE}" \
+"${GRUB_IMAGE_ROOT}"
+
+remove "${GRUB_IMAGE_ROOT}"
+}
+
 function grub_make_image {
 local architecture="${1}"
 local file="${2}"
--- lib/grubash.sh
+++ lib/grubash.sh
@@ -9,28 +9,6 @@ remove \
 "${GRUBASH_BIOS_DIRECTORY}"
 }
 
-function grubash_make_image {
-local esp_uuid="${1}"
-
-remove "${GRUB_IMAGE_ROOT}"
-make_directory "${GRUB_IMAGE_DIRECTORY}"
-
-echo -n "\
-search --set root --fs-uuid ${esp_uuid}
-unset prefix
-pager=1
-normal
-" >> "${GRUB_IMAGE_FILE}"
-
-tar \
---create \
---auto-compress \
---file "${GRUB_IMAGE_ARCHIVE}" \
-"${GRUB_IMAGE_ROOT}"
-
-remove "${GRUB_IMAGE_ROOT}"
-}
-
 function grubash_clean {
 remove \
 "${GRUB_IMAGE_ARCHIVE}"

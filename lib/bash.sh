78494cf461d81da1391ccb5e317cee46211f6a31
Marc Beninca <grubash@rwx.work>
2021-11-08 13:20:15 +0100

    bash_action

--- lib/bash.sh
+++ lib/bash.sh
@@ -1,3 +1,19 @@
+BASH_ACTION="${1}"
+BASH_FUNCTION='function'
+
+BASH_ACTIONS="$(\
+grep "${BASH_FUNCTION}" "${BASH_FILE}" \
+| awk '{print $2}' \
+)"
+
+function bash_action {
+if ! eval "${BASH_ACTION}" 2> /dev/null; then
+    for action in "${BASH_ACTIONS[@]}"; do
+        echo "${action}"
+    done
+fi
+}
+
 function make_directory {
 mkdir \
 --parents \

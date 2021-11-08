ba2c57672ebd4d42411668173042ec4dd34fd5a6
Marc Beninca <grubash@rwx.work>
2021-11-08 03:35:01 +0100

    grubash_make_image

--- lib/grubash.sh
+++ lib/grubash.sh
@@ -6,12 +6,32 @@ GRUBASH_BIOS_SETUP="${GRUBASH_BIOS_DIRECTORY}/setup"
 function grubash_wipe {
 remove \
 "${EFI_ROOT}" \
-"${GRUBASH_BIOS_DIRECTORY}" \
+"${GRUBASH_BIOS_DIRECTORY}"
+}
+
+function grubash_make_image {
+local esp_uuid="${1}"
+
+remove "${GRUB_IMAGE_ROOT}"
+make_directory "${GRUB_IMAGE_DIRECTORY}"
+
+echo -n "\
+search --set root --fs-uuid ${esp_uuid}
+unset prefix
+pager=1
+normal
+" >> "${GRUB_IMAGE_FILE}"
+
+tar \
+--create \
+--auto-compress \
+--file "${GRUB_IMAGE_ARCHIVE}" \
 "${GRUB_IMAGE_ROOT}"
+
+remove "${GRUB_IMAGE_ROOT}"
 }
 
 function grubash_clean {
 remove \
-"${GRUB_IMAGE_ARCHIVE}" \
-"${GRUB_IMAGE_ROOT}"
+"${GRUB_IMAGE_ARCHIVE}"
 }

ce7c3732d04a683d37bf3ccbac3aa31d84a87a35
Marc Beninca <grubash@rwx.work>
2021-11-12 21:59:26 +0100

    log

---
+++ bash/log.sh
@@ -0,0 +1,20 @@
+LOG_LEVEL_DEBUG=1
+LOG_LEVEL_INFO=2
+LOG_LEVEL_WARNING=3
+LOG_LEVEL_ERROR=4
+
+function log_debug {
+[ ${LOG_LEVEL} -le ${LOG_LEVEL_DEBUG} ] && echo "${@}"
+}
+
+function log_error {
+[ ${LOG_LEVEL} -le ${LOG_LEVEL_ERROR} ] && echo "${@}"
+}
+
+function log_info {
+[ ${LOG_LEVEL} -le ${LOG_LEVEL_INFO} ] && echo "${@}"
+}
+
+function log_warning {
+[ ${LOG_LEVEL} -le ${LOG_LEVEL_WARNING} ] && echo "${@}"
+}
